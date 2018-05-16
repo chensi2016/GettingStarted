66b59bf48a6951369452639987bbf970563c8711
admin <chensi910@gmail.com>
2018-05-16 16:50:50 +0900

    second commit

--- src/com/intellijava/Main.java
+++ src/com/intellijava/Main.java
@@ -6,15 +6,22 @@ public class Main {
         MyMap map= new MyMap();
         map.add("this",1 );
         map.add("coder",2 );
-        map.add("this",4 );
-        map.add("this",5 );
-        map.add("that",6 );
-        map.add("coder",3 );
-        map.add("tokyo",3 );
+        map.add("that",3 );
+        map.add("big",4 );
+        map.add("small",5 );
+        map.add("red",6 );
+        map.add("tokyo",7 );
+        map.add("osaka",8 );
+        map.add("koto",9 );
+        map.add("tsukishima",10 );
+        map.add("kachidoki",11 );
         System.out.println(map.getSize());
         System.out.println(map.remove("this"));
         System.out.println(map.remove("this"));
-        System.out.println(map.remove("this"));
+        System.out.println(map.getSize());
+        map.add("this",12);
+        System.out.println(map.getValue("this"));
+        System.out.println(map.getValue("tokyo"));
         System.out.println(map.getSize());
         System.out.println(map.isEmpty());
     }
