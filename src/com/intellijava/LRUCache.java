d13cd1c0177bd86df121e3c54514bc10a7ae7119
admin <chensi910@gmail.com>
2018-05-24 16:52:16 +0900

    two direction list & hashmap version of LUR Cache

--- src/com/intellijava/LRUCache.java
+++ src/com/intellijava/LRUCache.java
@@ -1,14 +1,16 @@
 package com.intellijava;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 class QNode{
     int key;
     int value;
     QNode next;
+    QNode prev;
     QNode(int key, int value){
         this.key=key;
         this.value=value;
         this.next=null;
+        this.prev=null;
     }
     QNode(){
 
@@ -17,15 +19,20 @@ class QNode{
 public class LRUCache {
     int capacity;
     QNode headNode;
+    QNode lastNode;
     int size=0;
+    HashMap<Integer,QNode> map=new HashMap<>();
     public LRUCache(int capacity) {
         this.capacity=capacity;
         headNode=new QNode();
+        lastNode=null;
       }
-
+    public QNode getRefer(Integer key){
+       return map.get(key);
+    }
     public int get(int key) {
         //check whether it is present
-        QNode pointerNode;
+       /* QNode pointerNode;
         pointerNode=headNode.next;
         QNode prevNode=headNode;
         QNode newNode=new QNode();
@@ -48,12 +55,35 @@ public class LRUCache {
             pointerNode=pointerNode.next;
         }
         return -1;
+        */
+       QNode foundNode=getRefer(key);
+       if (foundNode == null){
+           return -1;
+       }
+       else{
+           //if the foundNode is not the last Node;
+           if (foundNode.next != null ) {
+               foundNode.next.prev = foundNode.prev;
+               foundNode.prev.next = foundNode.next;
+               map.remove(key);
+               QNode newNode = new QNode(key, foundNode.value);
+               newNode.prev = lastNode;
+               lastNode.next = newNode;
+               lastNode = newNode;
+               map.put(key,newNode);
+               return lastNode.value;
+           }
+           else {//if the foundNode is the last Node, nothing changed;
+               return foundNode.value;
+           }
 
+       }
     }
 
     public void put(int key, int value) {
+
         //check whether it is present
-        QNode pointerNode;
+       /* QNode pointerNode;
         pointerNode=headNode.next;
         QNode prevNode=headNode;
         QNode newNode=new QNode(key,value);
@@ -78,6 +108,46 @@ public class LRUCache {
         if(size>capacity){
             headNode.next=headNode.next.next;
         }
+*/
 
+       //check whether the node is present
+        QNode foundNode = getRefer(key);
+        if( foundNode != null){
+            if (foundNode.next != null ) {
+                foundNode.value=value;
+                foundNode.next.prev = foundNode.prev;
+                foundNode.prev.next = foundNode.next;
+                foundNode.next = null;
+                foundNode.prev = lastNode;
+                lastNode.next = foundNode;
+                lastNode = foundNode;
+                return;
+            }
+            else {//if the foundNode is the last Node, just chang the value;
+                foundNode.value = value;
+                return;
+            }
+
+        }
+        else {
+            //insert the new node
+            QNode newNode = new QNode(key, value);
+            if(lastNode!=null) {
+                lastNode.next = newNode;
+                newNode.prev = lastNode;
+            }
+            else{
+                headNode.next = newNode;
+                newNode.prev = headNode;
+            }
+            size++;
+            lastNode = newNode;
+            map.put(key, newNode);
+            if(size>capacity){
+                map.remove(headNode.next.key);
+                headNode.next=headNode.next.next;
+                size--;
+            }
+        }
     }
 }
